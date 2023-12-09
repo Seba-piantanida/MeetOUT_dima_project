b08b9adc6e426dbdc04429d1fbf25ecd257f13e5
Seba_Piantanida <sebastiano.piantanida@mail.polimi.it>
2023-12-09 18:45:00 +0100

    custom marker and details added

---
+++ lib/events_manager.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+final List events = [
+  {
+    "id": "1",
+    "name": "Dog walking",
+    "description": "A walk with our furry friends :)",
+    "location": const LatLng(45.46427, 9.18951),
+    "date-time": DateTime(2023, 12, 7, 15, 30),
+    "icon": "assets/events_icons/dog-face_1f436.png",
+  },
+  {
+    "id": "2",
+    "name": "Bowling night",
+    "description": "Let's score some strike tonight",
+    "location": const LatLng(45.46427, 9.20),
+    "date-time": DateTime(2023, 12, 7, 15, 30),
+    "icon": "assets/events_icons/bowling_1f3b3.png",
+  },
+];
+
+List<dynamic> getEvents() {
+  return events;
+}
--- lib/find_page.dart
+++ lib/find_page.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi' as ui;
+
+import 'package:dima_project/events_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:intl/intl.dart';
 
 class FindPage extends StatefulWidget {
   const FindPage({super.key});
@@ -9,7 +13,9 @@ class FindPage extends StatefulWidget {
 }
 
 class _FindPageState extends State<FindPage> {
+  dynamic selectedEvents;
   late GoogleMapController mapController;
+  final Map<String, Marker> _markers = {};
 
   final LatLng _center = const LatLng(45.46427, 9.18951);
 
@@ -18,11 +24,110 @@ class _FindPageState extends State<FindPage> {
   }
 
   @override
+  void initState() {
+    _generateMarkers();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GoogleMap(
+    return Stack(children: [
+      GoogleMap(
         zoomControlsEnabled: false,
         mapToolbarEnabled: false,
         onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(target: _center, zoom: 11));
+        initialCameraPosition: CameraPosition(target: _center, zoom: 14),
+        markers: _markers.values.toSet(),
+      ),
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: selectedEvents != null
+              ? EventDetails(selectedEvents)
+              : SizedBox.expand(),
+        ),
+      )
+    ]);
+  }
+
+  _generateMarkers() async {
+    for (int i = 0; i < events.length; i++) {
+      BitmapDescriptor markerIcon = await BitmapDescriptor.fromAssetImage(
+          const ImageConfiguration(), events[i]["icon"]);
+
+      _markers[i.toString()] = Marker(
+          markerId: MarkerId(events[i]["id"]),
+          position: events[i]["location"],
+          icon: markerIcon,
+          onTap: () {
+            selectedEvents = events[i];
+            print(events[i]);
+            print("selected event: $selectedEvents");
+            setState(() {});
+          });
+
+      setState(() {});
+    }
+  }
+}
+
+class EventDetails extends StatelessWidget {
+  final dynamic event;
+  const EventDetails(this.event, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Card(
+          elevation: 20,
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Image.asset(
+                  event["icon"],
+                  scale: 2,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        event["name"],
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                      Expanded(
+                        child: Text(
+                          event["description"],
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(DateFormat("dd-MM-yy  hh:mm")
+                              .format(event["date-time"])
+                              .toString()),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {}, child: const Text("Join"))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
